8efd33c7791ea197f8a617f166dbd7ad2e91db45
mucha17 <46112158+mucha17@users.noreply.github.com>
2020-11-25 16:38:49 +0100

    Creating layout and index page

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>Projekty</h1>
+        </div>
+    </div>
+    <hr />
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <p>Zawartość testowa</p>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Menadżer projektów</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContents" aria-controls="navbarContents" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarContents">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Strona domowa</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Moje projekty</a>
+            </li>
+        </ul>
+    </div>
+</nav>
